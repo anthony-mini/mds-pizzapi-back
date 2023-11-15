684e1506f40d7675a8f2b7d89859fcf46de6dbbb
anthony-mini <anthony-mini@outlook.fr>
2023-11-15 10:09:55 +0100

    create module pizzas && install typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PizzasModule } from './pizzas/pizzas.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: 'pizzapi',
+      username: 'root',
+      password: 'root',
+      port: 8889,
+      host: 'localhost',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ConfigModule.forRoot(),
+    PizzasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
