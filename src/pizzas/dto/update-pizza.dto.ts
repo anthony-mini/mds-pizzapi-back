f2da442078885cb906b708a44554486b6bde9d7a
anthony-mini <anthony-mini@outlook.fr>
2023-11-15 10:41:21 +0100

    fix: dto update. hérite de CreatePizzaDto

--- src/pizzas/dto/update-pizza.dto.ts
+++ src/pizzas/dto/update-pizza.dto.ts
@@ -1,11 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePizzaDto } from './create-pizza.dto';
-import { PizzaFlavor } from '../enums/pizza-flavor.enum';
 
-export class UpdatePizzaDto extends PartialType(CreatePizzaDto) {
-  name: string;
-  flavor: PizzaFlavor;
-  speciality: boolean;
-  price: number;
-  end?: Date;
-}
+export class UpdatePizzaDto extends PartialType(CreatePizzaDto) {}
