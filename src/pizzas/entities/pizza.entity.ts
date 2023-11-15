74a374e4c0d4fce70e4081d94e76dda083a31ca4
anthony-mini <anthony-mini@outlook.fr>
2023-11-15 10:16:54 +0100

    Create pizza entity

--- src/pizzas/entities/pizza.entity.ts
+++ src/pizzas/entities/pizza.entity.ts
@@ -1,1 +1,23 @@
-export class Pizza {}
+import { Entity, PrimaryGeneratedColumn, Index, Column } from 'typeorm';
+
+@Entity()
+export class Pizza {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index({ unique: true })
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ default: false })
+  speciality: boolean;
+
+  @Column()
+  price: number;
+
+  @Column({ nullable: true })
+  end?: Date;
+}
