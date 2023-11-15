77b7b533b9a75b4debd352d6da7c12a8a038d41e
anthony-mini <anthony-mini@outlook.fr>
2023-11-15 11:57:47 +0100

    feat: TP2 Validation

--- src/pizzas/dto/create-pizza.dto.ts
+++ src/pizzas/dto/create-pizza.dto.ts
@@ -1,9 +1,19 @@
+import { MaxLength } from 'class-validator';
 import { PizzaFlavor } from '../enums/pizza-flavor.enum';
-
+import { IsNotEmpty, IsDefined, Min } from 'class-validator';
 export class CreatePizzaDto {
+  @IsNotEmpty()
+  @MaxLength(100)
   name: string;
+
+  @IsDefined()
   flavor: PizzaFlavor;
+
   speciality: boolean;
+
+  @IsDefined()
+  @Min(0)
   price: number;
+
   end?: Date;
 }
